d2f7ad5ac6fb030a61618ac0adab09a2c7fdaf63
pedroccustodio <pedroccustodio@gmail.com>
2023-02-19 15:34:47 +0000

    Library change

--- app/src/main/java/com/example/advertisinglibrary/MainActivity.kt
+++ app/src/main/java/com/example/advertisinglibrary/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.advertisinglibrary
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
-import com.example.banneradmaker.AddNumbers
+import com.example.banneradmaker.library
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Library(3,4)
+        library(this)
 
         //AlertDialog.Builder(this).setTitle("The result is $result").show()
 
