253dcf4875c7a86eb04f023f6f6c42aaa59a2430
pedroccustodio <pedroccustodio@gmail.com>
2023-02-19 18:55:31 +0000

    Library change

--- app/src/main/java/com/example/advertisinglibrary/MainActivity.kt
+++ app/src/main/java/com/example/advertisinglibrary/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.advertisinglibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.example.banneradmaker.library
 
@@ -10,9 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        library(this)
+        val tvDisplay: TextView = findViewById<TextView>(R.id.tv_displayInfo)
+        tvDisplay.text = library()
+
+
 
         //AlertDialog.Builder(this).setTitle("The result is $result").show()
-
     }
 }
+
--- bannerAdMaker/src/main/java/com/example/banneradmaker/BannerAdMaker.kt
+++ bannerAdMaker/src/main/java/com/example/banneradmaker/BannerAdMaker.kt
@@ -1,12 +1,46 @@
 package com.example.banneradmaker
 
+//noinspection SuspiciousImport
+import android.R
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ContentResolver
 import android.content.Context
+import android.os.Build
+import android.provider.Settings
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
 
-fun library (context: Context) {
+@SuppressLint("HardwareIds")
+fun library (): String {
 
-    AlertDialog.Builder(context).setTitle("Title")
+    return "Brand: ${Build.BRAND} \n" +
+            "Model: ${Build.MODEL} \n" +
+            "ID: ${Build.ID} \n" +
+            "SDK: ${Build.VERSION.SDK_INT} \n" +
+            "Manufacture: ${Build.MANUFACTURER} \n" +
+            "Brand: ${Build.BRAND} \n" +
+            "User: ${Build.USER} \n" +
+            "Type: ${Build.TYPE} \n" +
+            "Base: ${Build.VERSION_CODES.BASE} \n" +
+            "Incremental: ${Build.VERSION.INCREMENTAL} \n" +
+            "Board: ${Build.BOARD} \n" +
+            "Host: ${Build.HOST} \n" +
+            "FingerPrint: ${Build.FINGERPRINT} \n" +
+            "Version Code: ${Build.VERSION.RELEASE}"
+}
+    /*AlertDialog.Builder(context).setTitle("Title")
         .setMessage("Library in use").show()
 
-    return
-}
+    val a: Activity = context as Activity
+    a.setContentView(com.example.banneradmaker.R.layout.activity_main)
+
+    val imgView: ImageView = context.findViewById<View>(com.example.banneradmaker.R.id.imageView) as ImageView
+
+    imgView.visibility = ImageView.VISIBLE
+
+
+    return Build.VERSION.RELEASE*/
+
