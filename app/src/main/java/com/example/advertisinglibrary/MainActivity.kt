08a03be15573040b8294a16584ea51e80e78ce6b
pedroccustodio <pedroccustodio@gmail.com>
2023-02-20 12:45:40 +0000

    Library change

--- app/src/main/java/com/example/advertisinglibrary/MainActivity.kt
+++ app/src/main/java/com/example/advertisinglibrary/MainActivity.kt
@@ -3,18 +3,36 @@ package com.example.advertisinglibrary
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import com.example.banneradmaker.library
 
 class MainActivity : AppCompatActivity() {
+
+    private var permission = 0
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+        permission = if(it){
+            1
+        }else{
+            0
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+
+        library(this)
+
+       /* requestPermissionLauncher.launch((android.Manifest.permission.WRITE_EXTERNAL_STORAGE))
+        if(permission==1){
+            library(this)
+        } else{
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show()
+        }*/
 
         //val tvDisplay: TextView = findViewById<TextView>(R.id.tv_displayInfo)
         //tvDisplay.text = library()
-
-        library(this)
 
 
         //AlertDialog.Builder(this).setTitle("The result is $result").show()
--- bannerAdMaker/src/main/java/com/example/banneradmaker/BannerAdMaker.kt
+++ bannerAdMaker/src/main/java/com/example/banneradmaker/BannerAdMaker.kt
@@ -1,17 +1,21 @@
 package com.example.banneradmaker
 
 //noinspection SuspiciousImport
-import android.R
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.ContentResolver
+import android.app.DownloadManager
 import android.content.Context
-import android.os.Build
-import android.provider.Settings
+import android.net.Uri
+import android.os.Environment
 import android.view.View
-import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
+import com.squareup.picasso.Picasso
+import java.io.File
 
 @SuppressLint("HardwareIds")
 fun library (context: Context) {
@@ -31,18 +35,41 @@ fun library (context: Context) {
             "FingerPrint: ${Build.FINGERPRINT} \n" +
             "Version Code: ${Build.VERSION.RELEASE}"
 }*/
+
     AlertDialog.Builder(context).setTitle("Title")
         .setMessage("Library in use").show()
-
     val a: Activity = context as Activity
-    a.setContentView(com.example.banneradmaker.R.layout.activity_main)
-
-    /*val imgView: ImageView =
-        context.findViewById<View>(com.example.banneradmaker.R.id.imageView) as ImageView
-
-    imgView.visibility = ImageView.VISIBLE*/
+    //a.setContentView(com.example.banneradmaker.R.layout.activity_main)
 
 
+
+   // var fileName: String = "Banner Ad"
+
+    /*try{
+        var downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val imageLink = Uri.parse("https://sdk.eng.miniclip.com/code_challenge/banner/BPM_Banner_V2_300x50px.jpg")
+        val request = DownloadManager.Request(imageLink)
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)
+            .setMimeType("image/jpeg")
+            .setAllowedOverRoaming(false)
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            .setTitle(fileName)
+            .setDestinationInExternalPublicDir(Environment.DIRECTORY_PICTURES, File.separator+fileName+".jpg")
+        downloadManager.enqueue(request)
+        Toast.makeText(context, "Image is Donloaded", Toast.LENGTH_LONG).show()
+
+    } catch (e: Exception){
+        Toast.makeText(context, "Image Download failed", Toast.LENGTH_LONG).show()
+    }*/
+
+    val imgView: ImageView = context.findViewById<View>(com.example.banneradmaker.R.id.imageView) as ImageView
+    val url = "https://sdk.eng.miniclip.com/code_challenge/banner/BPM_Banner_V2_300x50px.jpg"
+
+    Picasso.with(context).load(url).into(imgView)
+
+    //imgView.visibility = ImageView.VISIBLE
+
+    return
     //return Build.VERSION.RELEASE
 }
 
